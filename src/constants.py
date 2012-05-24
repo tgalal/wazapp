8916f3a8edc3bc47670453616310d17a38373f0b
tarekg <tarekg@tarek-gentoo.(none)>
2012-05-24 02:09:00 +0200

    Cont last commit

--- src/constants.py
+++
@@ -1,8 +0,0 @@
-class WAConstants():
-	CLIENT_INSTALL_PATH = '/opt/waxmppplugin/bin/wazapp'
-	DEFAULT_CONTACT_PICTURE = CLIENT_INSTALL_PATH+'/'+'UI/pics/user.png';
-	DEFAULT_SOUND_NOTIFICATION = "/usr/share/sounds/ring-tones/Message 1.mp3"
-	FOCUSED_SOUND_NOTIFICATION = "/usr/share/sounds/ui-tones/snd_default_beep.wav"
-	DEFAULT_BEEP_NOTIFICATION = "/usr/share/sounds/ui-tones/snd_default_beep.wav"
-	NO_SOUND = "/usr/share/sounds/ring-tones/No sound.wav"
-	
--- src/exceptions.py
+++
@@ -1,2 +0,0 @@
-class ConnectionClosedException(Exception):
-    pass

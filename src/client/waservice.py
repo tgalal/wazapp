7ac198f987e58ca3bcd1f45187a042d7b95e8b3d
tarekg <tarekg@tarek-gentoo.(none)>
2012-05-25 03:52:49 +0200

    license ammend

--- src/client/waservice.py
+++ src/client/waservice.py
@@ -2,22 +2,20 @@
 '''
 Copyright (c) 2012, Tarek Galal <tarek@wazapp.im>
 
-This file is part of Wazapp, an IM application for Meego Harmattan platform that allows communication with Whatsapp users
+This file is part of Wazapp, an IM application for Meego Harmattan platform that
+allows communication with Whatsapp users
 
-Wazapp is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Wazapp is free software: you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software 
+Foundation, either version 3 of the License, or (at your option) any later 
+version.
 
-Wazapp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+Wazapp is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with Wazapp. If not, see http://www.gnu.org/licenses/.
-'''
-
-'''
-This file is a derivative of Python Event Feed library by Thomas Perl 
-Copyright (c) 2011, Thomas Perl <m@thp.io>
-
-Permission to use, copy, modify, and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
+You should have received a copy of the GNU General Public License along with 
+Wazapp. If not, see http://www.gnu.org/licenses/.
 '''
 # Dependency on PySide for encoding/decoding like MRemoteAction
 from PySide.QtCore import QBuffer, QIODevice, QDataStream, QByteArray

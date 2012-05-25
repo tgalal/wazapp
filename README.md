2822c27590eb5a3d0a71431c9d75e69f3da1ec6c
tarekg <tarekg@tarek-gentoo.(none)>
2012-05-25 13:10:40 +0200

    License change

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Wazapp
 Wazapp is an IM application for N9 that allows you to chat with your Whatsapp buddies
 
 Wazapp consists of 2 parts, the IM application (wazapp), which is in Python, 
-and an accounts plugin (waxmppplugin) in C++. The accounts plugin
+and the accounts plugin (waxmppplugin) in C++. The accounts plugin
 should be rewritten in python soon. 
 
 Homepage: http://www.wazapp.im
@@ -17,7 +17,7 @@ License:
 
 	Wazapp is free software: you can redistribute it and/or modify it under the terms 
 	of the GNU General Public License as published by the Free Software Foundation, 
-	either version 3 of the License, or (at your option) any later version.
+	either version 2 of the License, or (at your option) any later version.
 
 	Wazapp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
 	without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
--- src/client/mnotification.py
+++ src/client/mnotification.py
@@ -7,7 +7,7 @@ allows communication with Whatsapp users
 
 Wazapp is free software: you can redistribute it and/or modify it under the 
 terms of the GNU General Public License as published by the Free Software 
-Foundation, either version 3 of the License, or (at your option) any later 
+Foundation, either version 2 of the License, or (at your option) any later 
 version.
 
 Wazapp is distributed in the hope that it will be useful, but WITHOUT ANY 
@@ -16,6 +16,27 @@ PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with 
 Wazapp. If not, see http://www.gnu.org/licenses/.
+'''
+# -*- coding: utf-8 -*-
+
+'''
+Copyright (c) 2012, Tarek Galal <tare2.galal@gmail.com>
+
+This file is part of python-notifications, a library that allows you to post 
+notifications on Harmattan platform
+
+python-notifications is free software: you can redistribute it and/or modify it 
+under the terms of the GNU Lesser General Public License as published by the 
+Free Software Foundation, either version 3 of the License, or (at your option) 
+any later version.
+
+python-notifications is distributed in the hope that it will be useful, but WITHOUT ANY 
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+details.
+
+You should have received a copy of the GNU General Public License along with 
+python-notifications. If not, see http://www.gnu.org/licenses/.
 '''
 
 '''
